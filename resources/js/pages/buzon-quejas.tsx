8131833c36f9cfee79ab3cce4b0064395765f4ac
vvillalva <victor.villalva0326@gmail.com>
2025-07-22 12:40:07 -0600

    Feat: Implementación de metodo create para que genere la queja el formulario

--- resources/js/pages/buzon-quejas.tsx
+++ resources/js/pages/buzon-quejas.tsx
@@ -1,7 +1,15 @@
+
 import FormularioBuzon from "@/components/buzon/formulario-buzon";
 import Header from "@/components/header";
+import { AlertDialog, AlertDialogAction, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from "@/components/ui/alert-dialog";
+import { usePage } from "@inertiajs/react";
+import { useState } from "react";
 
 export default function BuzonQuejas() {
+  const [open, setOpen] = useState(true);
+  const { props } = usePage<{ success?: string; folio?: string }>();
+  const success = props.success;
+  const folio = props.folio;
   return (
     <main className="h-screen">
       <Header />
@@ -12,16 +20,35 @@ export default function BuzonQuejas() {
               {/* Por si se agrega icono aun lado */}
               <h1 className="font-semibold text-4xl text-[#96559B] dark:text-white">Buzón de quejas</h1>
             </div>
-            <hr className="mx-6 h-12 w-px bg-[#BEB9B9] border-0 hidden lg:flex dark:text-white"/>
+            <hr className="mx-6 h-12 w-px bg-[#BEB9B9] border-0 hidden lg:flex dark:text-white" />
             <p className="text-[#434343] font-light text-base dark:text-white">Unidad de Igualdad de Genero</p>
           </div>
-          <hr className="bg-[#F2EDED]"/>
+          <hr className="bg-[#F2EDED]" />
           <p className="text-sm text-[#4E4E4E] dark:text-white">Este buzón es un espacio seguro donde puedes reportar cualquier situación de violencia de género que hayas sufrido o presenciado. Toda la información se manejará con estricta confidencialidad y se dará seguimiento conforme a la normativa vigente.</p>
           <p className="text-sm text-[#96559B] font-semibold italic dark:text-white">*Tus datos personales son opcionales. Si prefieres mantener el anonimato, puedes omitir los campos de identificación. Tu seguridad y privacidad son nuestra prioridad.</p>
         </div>
         <div className="formulario">
           <FormularioBuzon />
         </div>
+        {success && folio && (
+          <AlertDialog open={open} onOpenChange={setOpen}>
+            {/* No necesitas AlertDialogTrigger */}
+            <AlertDialogContent>
+              <AlertDialogHeader>
+                <AlertDialogTitle className="text-2xl flex flex-row items-center lg:justify-start justify-center gap-2"> ¡Queja enviada!</AlertDialogTitle>
+                <AlertDialogDescription className="font-semibold">
+                  {success} A continuación se te dara tu folio de seguimiento para tu casa, recuerda guardar bien los siguientes datos.<br /><br />
+                  <span>Num. de folio: <br /> <span className="font-bold text-xl">{folio}</span></span>
+                </AlertDialogDescription>
+              </AlertDialogHeader>
+              <AlertDialogFooter>
+                <AlertDialogAction onClick={() => setOpen(false)}> 
+                  Confirmar
+                </AlertDialogAction>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialog>
+        )}
       </section>
     </main>
   )
--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,23 @@
 <?php
 
 use App\Http\Controllers\CatalogoController;
+use App\Http\Controllers\QuejaController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('buzon-quejas');
-})->name('buzon-quejas');
+    return Inertia::render(component: 'buzon-quejas');
+})->name('buzon');
+
+Route::post('/', [QuejaController::class, 'store'])->name('buzon.store');
+
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('dashboard', function () {
-        return Inertia::render('dashboard');
-    })->name('dashboard');
+    // Route::get('dashboard', function () {
+    //     return Inertia::render('dashboard');
+    // })->name('dashboard');
+
+    Route::get('dashboard', [QuejaController::class, 'index']);
 
     Route::get('estadisticas', function () {
         return Inertia::render('estadisticas');
