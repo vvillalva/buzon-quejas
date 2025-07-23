d61519c44bd01c95f7149e2a67106edcf5308b3e
vvillalva <victor.villalva0326@gmail.com>
2025-07-23 08:45:20 -0600

    Feat: Data Table para la lista de opciones

--- resources/js/components/buzon/columns.tsx
+++ resources/js/components/buzon/columns.tsx
@@ -1,13 +1,14 @@
 import { ColumnDef } from "@tanstack/react-table"
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 import { Button } from "../ui/button";
-import { ArrowUpDown, EllipsisVertical } from "lucide-react";
+import { ArrowUpDown, CircleCheck, EllipsisVertical, Loader } from "lucide-react";
 import { Link, router } from "@inertiajs/react";
+import { Badge } from "../ui/badge";
 
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
@@ -16,8 +17,8 @@ export type ColumnaCatalogo = {
     nombre: string
 }
 
-function handleDelete(id:number){
-    if(confirm("Estas seguro de eliminar el dato?")){
+function handleDelete(id: number) {
+    if (confirm("Estas seguro de eliminar el dato?")) {
         router.delete(route('catalogos.destroy', id))
     }
 }
@@ -176,4 +177,70 @@ export const columnasQuejas: ColumnDef<ColumnaQueja>[] = [
         },
         cell: ({ row }) => <div className="w-full pl-3"><span>{row.getValue('mensaje')}</span></div>
     }
+]
+
+export type ColumnaOpcion = {
+    id: number;
+    nombre: string;
+    estatus: string;
+}
+
+export const columnasOpciones: ColumnDef<ColumnaOpcion>[] = [
+        {
+        accessorKey: "nombre",
+        header: ({ column }) => {
+            return (
+                <Button
+                    variant="ghost"
+                    onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+                >
+                    Nombre de opción
+                    <ArrowUpDown />
+                </Button>
+            )
+        },
+        cell: ({ row }) => <div className="w-full pl-3"><span>{row.getValue('nombre')}</span></div>
+    },
+    {
+        accessorKey: "estatus",
+        header: "Estatus de Opción",
+        cell: ({ row }) => (
+            <Badge variant="outline" className="text-muted-foreground px-1.5">
+                {row.original.estatus === "1" ? (
+                    <>
+                        <CircleCheck className="fill-green-500 dark:fill-green-400" />
+                        Activo
+                    </>
+                ) : (
+                    <>
+                        <Loader />
+                        Desactivado
+                    </>
+                )}
+            </Badge>
+        ),
+    },
+    {
+        id: "actions",
+        cell: ({ row }) => (
+            <div className="flex flex-row justify-end">
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button
+                            variant="ghost"
+                            className="data-[state=open]:bg-muted text-muted-foreground flex size-8"
+                            size="icon"
+                        >
+                            <EllipsisVertical />
+                            <span className="sr-only">Opciones</span>
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end" className="w-32">
+                        <DropdownMenuItem ><Link href="">Editar</Link></DropdownMenuItem>
+                        <DropdownMenuItem variant="destructive" onClick={() => handleDelete(row.original.id)}>Borrar</DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            </div>
+        ),
+    },
 ]
--- resources/js/pages/catalogo/lista-opciones.tsx
+++ resources/js/pages/catalogo/lista-opciones.tsx
@@ -1,4 +1,4 @@
-import { ColumnaCatalogo, columnasCatalogo } from '@/components/buzon/columns';
+import { ColumnaCatalogo, columnasCatalogo, columnasOpciones } from '@/components/buzon/columns';
 import { DataTable }  from '@/components/buzon/data-table';
 import Encabezados from '@/components/buzon/encabezados';
 import { Button } from '@/components/ui/button';
@@ -30,11 +30,13 @@ export default function ListaOpciones({ opciones = [], resourceName = "" }) {
             <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-8 overflow-x-auto">
                 <Encabezados title={`${TITULOS[resourceName] ?? "Catalogo"}`} subtitle='Revisa las diferentes opciones que contiene tu catalogo dentro de todo el sistema.' />
                 <Link href={route('tipo-de-violencia.create')} className="px-3 py-1 bg-primary text-primary-foreground shadow-xs hover:bg-primary/90 rounded flex flex-row items-center gap-1"> <Plus size={16} /> Nueva opcion</Link>
-
-                {/* <DataTable columns={columnasCatalogo} data={catalogos}/> */}
+                <DataTable 
+                    columns={columnasOpciones} 
+                    data={opciones}
+                    placeholderFilter='Buscar por nombre...'
+                    filter='nombre'
+                />
             </div>
         </AppLayout>
     )
 }
-
-//TODO: Generar la tabla para las opciones
