4592bafda4a88fc11ea4c218dd9ab5f22399cf0c
vvillalva <victor.villalva0326@gmail.com>
2025-07-23 15:45:45 -0600

    Inicialización de graficas

--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -1,6 +1,8 @@
+import ChartDashboard from '@/components/buzon/chart-dashboard';
 import { columnasQuejas } from '@/components/buzon/columns';
 import { DataTable } from '@/components/buzon/data-table';
 import Encabezados from '@/components/buzon/encabezados';
+import { SectionCards } from '@/components/section-cards';
 import AppLayout from '@/layouts/app-layout';
 import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/react';
@@ -12,23 +14,16 @@ const breadcrumbs: BreadcrumbItem[] = [
     },
 ];
 export default function Dashboard({ buzon=[] }) {
-    //TODO: Realizar CRUD de opciones, dependiendo su catalogo
+    //TODO: CONFIGURAR BIEN LOS DATOS ESTADISTICOS, investigar que datos estadisticos conviene sacar
     //TODO: Realizar CRUD para la vista de usuarios, 
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Dashboard" />
             <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4 overflow-x-auto">
                 <Encabezados title="Dashboard" subtitle="Revisa las quejas que se han genero y los datos estadisticos historicos." />
-                <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-                    <div className="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                        AQUI VA UNA GRAFICA
-                    </div>
-                    <div className="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                        aQUI VA OTRA GRAFICA
-                    </div>
-                    <div className="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                        aqui tmb va otra
-                    </div>
+                <div className="@container/main flex flex-1 flex-col gap-4">
+                    <SectionCards />
+                    <ChartDashboard />
                 </div>
                 <div className="relative min-h-[100vh] flex-1 overflow-hidden rounded-xl border border-sidebar-border/70 md:min-h-min dark:border-sidebar-border">
                     <DataTable
